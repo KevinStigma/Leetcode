66085b3b873d99c60630a6c63ed7fb3ef99adc7b
kevinstigma <781422447@qq.com>
2017-02-17 18:57:15 +0800

    Permutations

--- Permutations.py
+++ Permutations.py
@@ -1,7 +1,7 @@
 permutations=[]
 visited=[]
 one_permu=[]
-def DFS(nums,start_id):
+def DFS(nums):
 	global visited,one_permu,permutations
 	if len(one_permu)==len(nums):
 		#we can't use 'permutations.append(one_permu), because the value of one_permu will change later, so 
@@ -9,13 +9,11 @@ def DFS(nums,start_id):
 		permutations.append(list(one_permu))
 		return
 	for i in range(len(nums)):
-		if i==start_id:
-			continue
 		if visited[i]==True:
 			continue
 		visited[i]=True
 		one_permu.append(nums[i])
-		DFS(nums,i)
+		DFS(nums)
 		visited[i]=False
 		one_permu.pop()
 
@@ -25,14 +23,7 @@ class Solution(object):
 		one_permu=[]
 		permutations=[]
 		visited=[False for j in range(len(nums))]
-		for i in range(len(nums)):
-			for j in range(len(visited)):
-				visited[j]=False
-			one_permu=[nums[i]]
-			visited[i]=True
-			DFS(nums,i)
-			visited[i]=False
-			one_permu.pop()
+		DFS(nums)
 		return permutations
 s=Solution()
 print s.permute([1,2,3])
