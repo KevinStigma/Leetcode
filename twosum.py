4c3ca5a6dd3bb8eea31bd3c0b71d7a40adc7fe29
kevinstigma <781422447@qq.com>
2017-02-23 21:30:17 +0800

    two sum

--- twosum.py
+++ twosum.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def twoSum(self, nums, target):
+	def twoSum(self, nums, target):
 		hashMap={}
 		for i in range(len(nums)):		
 			n=target-nums[i]
@@ -10,6 +10,18 @@ class Solution(object):
 				else:
 					return [j,i]
 			hashMap[nums[i]]=i
-	
+	def twoSum2(self,nums,target):
+		sorted_index=sorted(range(len(nums)), key=lambda k: nums[k])
+		nums.sort()
+		low=0
+		high=len(nums)-1
+		while low<high:
+			if nums[low]+nums[high]==target:
+				return [sorted_index[low],sorted_index[high]]
+			elif nums[low]+nums[high]>target:
+				high=high-1
+			else:
+				low=low+1
+				
 solution=Solution()
-print solution.twoSum([3,2,4],6)
+print solution.twoSum2([3,2,4],6)
