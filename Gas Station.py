8d57c69b589ef34da00cbba16f83534388e01762
kevinstigma <781422447@qq.com>
2017-03-01 17:30:44 +0800

    gas station's dy solution

--- Gas Station.py
+++ Gas Station.py
@@ -67,3 +67,61 @@ class Solution(object):
 			return -1
 		else:
 			return start_id
+			
+	#use dynamic programming
+	def canCompleteCircuit4(self, gas, cost):
+		gas_n=len(gas)
+		diff=[0]*gas_n
+		for i in range(gas_n):
+			diff[i]=gas[i]-cost[i]
+		sum=0
+		for i in range(gas_n):
+			sum=sum+diff[i]
+		if sum<0:
+			return -1
+		p=[0]*gas_n
+		p[0]=diff[0]
+		max_value=p[0]
+		max_ind=0
+		for i in range(1,gas_n):
+			if p[i-1]+diff[i]>=diff[i]:
+				p[i]=p[i-1]+diff[i]
+			else:
+				p[i]=diff[i]
+			if max_value<p[i]:
+				max_ind=i
+				max_value=p[i]
+		max_list=[]
+		for i in range(max_ind,-1,-1):
+			if diff[i]<0:
+				break
+			max_list.append(i)
+		max_list.reverse()	
+		
+		p[0]*gas_n
+		p[0]=diff[0]
+		min_value=p[0]
+		min_ind=0
+		for i in range(1,gas_n):
+			if p[i-1]+diff[i]<=diff[i]:
+				p[i]=p[i-1]+diff[i]
+			else:
+				p[i]=diff[i]
+			if min_value>p[i]:
+				min_ind=i
+				min_value=p[i]
+		min_list=[]
+		for i in range(min_ind,-1,-1):
+			if diff[i]>0:
+				break
+			min_list.append(i)
+		min_list.reverse()
+		if max_value>sum-min_value:
+			return max_list[0]
+		else:
+			return (min_ind+1)%gas_n
+		
+		
+s=Solution()
+print s.canCompleteCircuit4([5],[4])
+			
