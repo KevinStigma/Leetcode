a873818767d2ac1d1882d3dba85fbf77a1de6b68
kevinstigma <781422447@qq.com>
2017-05-13 10:30:14 +0800

    2017.5.13

---
+++ Populating Next Right Pointers in Each Node.py
@@ -0,0 +1,33 @@
+# Definition for binary tree with next pointer.
+class TreeLinkNode:
+	def __init__(self, x):
+		self.val = x
+		self.left = None
+		self.right = None
+		self.next = None
+
+class Solution:
+	def connect(self, root):
+		if root==None:
+			return
+		root.next=None
+		begin=root
+		while not(begin.left==None or begin.right==None):
+			cur_node=begin
+			pre=None
+			while cur_node!=None:
+				cur_node.left.next=cur_node.right
+				if pre!=None:
+					pre.right.next=cur_node.left
+				pre=cur_node
+				cur_node=cur_node.next
+			pre.right=None
+			begin=begin.left
+				
+				
+			
+			
+			
+		
+		
+		
