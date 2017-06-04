df5ef1a6f622a730c3d2f49657d9469cf23d6f0e
kevinstigma <781422447@qq.com>
2017-06-04 18:30:30 +0800

    2017.6.4  in memory of the student strike of Tian'an Men's Square in 1989

---
+++ Palindrome Partitioning.py
@@ -0,0 +1,38 @@
+def isPalindrome(cur_str):
+	if len(cur_str)==0:
+		return False
+	begin=0
+	end=len(cur_str)-1
+	while begin<end:
+		if cur_str[begin]!=cur_str[end]:
+			return False
+		begin=begin+1
+		end=end-1
+	return True
+	
+class Solution(object):
+	def __init__(self):
+		self.results=[]
+	def backTracing(self,cur_str,cur_res):
+		for i in range(len(cur_str)):
+			tmp_str=cur_str[0:i+1]
+			if isPalindrome(tmp_str):
+				cur_res.append(tmp_str)
+				if i!=len(cur_str)-1:
+					self.backTracing(cur_str[i+1:],cur_res)
+				else:
+					self.results.append(cur_res[:])
+				cur_res.pop()
+	def partition(self, s):
+		self.results=[]
+		if len(s)==0:
+			return []
+		if len(s)==1:
+			return [[s[0]]]
+		self.backTracing(s,[])
+		return self.results
+
+s=Solution()
+print s.partition("aab")
+		
+		
